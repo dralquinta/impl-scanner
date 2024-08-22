36f4ad43f8b546879e97c52ac36c8805c4c41f25
Denny Alquinta <dralquinta@google.com>
2024-08-22 19:16:07 -0400

    Initial commit

---
+++ impl-scanner.py
@@ -0,0 +1,11 @@
+def __exec_orchestrator():
+    print("Hi")
+
+
+
+
+def __main__():
+    __exec_orchestrator()   
+
+
+__main__()
---
+++ impl-scanner.sh
@@ -0,0 +1,36 @@
+
+__install_prereqs(){
+    echo '============== Virtual Environment Creation =============='
+    python3 -m venv venv
+    source venv/bin/activate
+
+    echo '============== Upgrading pip3 =============='
+    pip3 install --upgrade pip
+
+    echo '============== Installing requirements =============='
+    pip3 install -r requirements.txt
+}
+
+
+
+
+__call_impl-scanner(){
+    python3 impl-scanner.py 
+}
+
+
+__start_impl-scanner(){
+
+    __call_impl-scanner
+
+}
+
+
+
+__main__(){ 
+    __install_prereqs
+    __start_impl-scanner
+}
+
+
+__main__
